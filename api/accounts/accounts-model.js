11287c731c55749c645f2837e6d5d8da43a9c5ad
Nicholas Carmack <dragecarmack@gmail.com>
2023-08-30 11:30:36 -0600

    completed mvp

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,28 @@
+const db = require("../../data/db-config.js");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db("accounts");
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const create =async (account) => {
+  const [id] = await db("accounts").insert(account);
+  return getById(id);
+};
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+const updateById =async (id, account) => {
+  await db("accounts").where("id", id).update(account);
+  return getById(id);
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const deleteById =async (id) => {
+  const removedAcocunt = getById(id);
+  await db("accounts").where("id", id).del();
+  return removedAcocunt;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +30,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,68 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Accounts = require("./accounts-model");
+const {
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+} = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", (req, res, next) => {
+  Accounts.getAll()
+    .then((account) => {
+      res.json(account);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", checkAccountId, (req, res, next) => {
+  Accounts.getById(req.params.id).then((account) => {
+    res.json(account);
+  });
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
+  (req, res, next) => {
+    const { name, budget } = req.body;
+    Accounts.create({
+      name: name.trim(),
+      budget: budget,
+    })
+      .then((newAccount) => {
+        res.status(201).json(newAccount);
+      })
+      .catch((err) => {
+        next();
+      });
+  }
+);
+
+router.put("/:id", checkAccountId, checkAccountPayload, (req, res, next) => {
+  Accounts.updateById(req.params.id, req.body)
+    .then((updatedAccount) => {
+      res.status(200).json(updatedAccount);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete("/:id", checkAccountId, (req, res, next) => {
+  Accounts.deleteById(req.params.id)
+    .then((removedAccount) => {
+      res.json(removedAccount);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+});
 
 module.exports = router;
